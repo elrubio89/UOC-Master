486b52f7299e94ee742bed349cf06a6d1ff30b8d
Marc Noguera Julian <mnoguera@mbpro-mnoguera.local>
2021-03-12 13:01:10 +0100

    Update test.sh

--- bashScripts/test.sh
+++ bashScripts/test.sh
@@ -14,3 +14,5 @@ echo "my Input File 1 is: $myInFile1"
 echo "my Input File 2 is: $myInFile2"
 
 echo "call script as: script.sh File1 File2"
+
+rgi run card_db $myInFile1 -out `basename $myInFile1`
